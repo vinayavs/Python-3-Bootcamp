7967b802a0ef534a7603bc847fa974f09992c863
Vinay AVS <vinayavs@gmail.com>
2020-06-12 16:28:40 -0500

    demo commit

---
+++ 01-readNumbers.py
@@ -0,0 +1,5 @@
+# Read numbers from CLI and aggregations 
+a = int(input('Enter first number :'))
+b = int(input('Enter second number :'))
+print("SUM IS :" ,a+b)
+print("PRODUCT IS :",a*b)
---
+++ 02-displayTriangledStar.py
@@ -0,0 +1,4 @@
+# DISPLAY TRIANGED *'S FOR GIVEN NUMBER
+a = int(input("ENTER A NUMBER TO DISPLAY TRIANGLED *'S :"))
+for i in range(1, a+1):
+    print('*' * i)    
