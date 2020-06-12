24c03eaf152b5454e06c14380a094d9ee2555095
Vinay AVS <vinayavs@gmail.com>
2020-06-12 17:52:45 -0500

    Right Triangle

--- 02-rightTriangledStar.py
+++ 02-rightTriangledStar.py
@@ -1,4 +1,4 @@
-# PRINT RIGHT TRIANGLE *'S FOR GIVEN NUMBER
+# PRINT RIGHT TRIANGLE *'S FOR GIVEN NUMBER.
 a = int(input("ENTER A NUMBER TO DISPLAY TRIANGLED *'S :"))
 for i in range(1, a+1):
     print('* ' * i)    
