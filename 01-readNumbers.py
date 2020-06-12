6fa460aa195c050f9d9d86cbada3e44cc9330f9b
Vinay AVS <vinayavs@gmail.com>
2020-06-12 17:51:02 -0500

    Read Numbers Dyanmiclly

--- 01-readNumbers.py
+++ 01-readNumbers.py
@@ -1,4 +1,4 @@
-# Read numbers from CLI and aggregations 
+# Read numbers from CLI and aggregations. 
 a = int(input('Enter first number :'))
 b = int(input('Enter second number :'))
 print("SUM IS :" ,a+b)
