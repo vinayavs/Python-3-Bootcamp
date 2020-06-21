31d213ff9e62714e41976446f028d3b8aa1872de
Vinay AVS <vinayavs@gmail.com>
2020-06-21 17:38:29 -0500

    Nested List - Matix

--- 30-nestedListMatrix.py
+++ 30-nestedListMatrix.py
@@ -1,8 +1,9 @@
 # PRINT NESTED LIST AS METRIX
 l = [[10,20,30],[40,50,60],[70,80,90]]
+print('####ELEMENTS BY ROW WISE####')
 for i in l:
     print(i) 
-print('####ELEMENTS BY ROW WISE####')
+print('####ELEMENTS IN MATRIX STYLE####')
 for i in l:
     for j in i:
         print(j,end=' ')
