a09384d557bf9af359465340430b8532dd7541fc
Vinay AVS <vinayavs@gmail.com>
2020-07-11 17:29:51 -0500

    Modifying Instance&Static Variables

--- 02-OOPs/16-modifyingStaticVar2.py
+++ 02-OOPs/16-modifyingStaticVar2.py
@@ -27,3 +27,44 @@ print('Test :',Test.a,Test.b) # 88,99
 # t1.b = 99
 # print('t1 :',t1.a,t1.b) #88 99
 # print('t2 :',t2.a,t2.b) #88 20
+
+
+
+# class Test:
+#     a = 10
+#     def __init__(self):
+#         self.b = 20
+#     def m1(self):
+#         self.a = 88
+#         self.b = 99
+#     @classmethod
+#     def m2(cls):
+#         cls.a = 66
+#         cls.b = 77
+        
+# t1 = Test()
+# t2 = Test()
+# t1.m1()
+# print('t1 :',t1.a,t1.b) # 88 99
+# print('t2 :',t2.a,t2.b) # 10 20
+
+# t2.m1()
+# print('t1 :',t1.a,t1.b) # 88 99
+# print('t2 :',t2.a,t2.b) # 88 99
+
+# class Test:
+#     a = 10
+#     def __init__(self):
+#         self.b = 20
+#     @classmethod
+#     def m1(cls):
+#         cls.a = 66
+#         cls.b = 77
+# t1 = Test()
+# t2 = Test()
+# t1.m1()  # calling classmethod with obj ref, but recommanded to use classname
+# print('t1 :',t1.a,t1.b) # 66 20
+# print('t2 :',t2.a,t2.b) # 66 20
+# print('Test :',Test.a,Test.b) # 66 77
+
+
