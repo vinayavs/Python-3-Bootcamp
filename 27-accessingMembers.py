e23a4a3f8999ed47c4fde83c8563d6fdff7d3311
Vinay AVS <vinayavs@gmail.com>
2020-07-13 16:51:22 -0500

    Accessing Member of a Class from inside class

---
+++ 27-accessingMembers.py
@@ -0,0 +1,15 @@
+# ACCESSING MEMBERS OF ONE CLASS INSIDE OF ANOTHER CLASS
+class Employee:
+    def __init__(self,name,sal): # Constructor
+        self.name = name
+        self.sal = sal
+    def display(self): #Instance Method
+        print('EMP NAME :',self.name)
+        print('EMP SAL :',self.sal)
+class Manager:
+    def updateSal(emp): # Static method
+        emp.sal = emp.sal+10000
+        emp.display()
+
+emp = Employee('vin',25000)
+Manager.updateSal(emp)
