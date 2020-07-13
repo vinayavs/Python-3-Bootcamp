d6cfa685102673ee09c031aaa970d0c2af2ec039
Vinay AVS <vinayavs@gmail.com>
2020-07-13 15:55:41 -0500

    Static Method Examples

---
+++ 02-OOPs/26-staticMethodEx.py
@@ -0,0 +1,9 @@
+# STATIC METHOD
+# INSTANCE OR STATIC VARAIBLES ARE NOT ALLOWED
+class Calc:
+    def add(a,b):
+        print(a+b)
+    def prod(a,b):
+        print(a*b)
+Calc.add(10,20)
+Calc.prod(10,2)
