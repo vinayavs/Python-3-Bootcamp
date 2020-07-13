2b3cf0bcccf293b029fe33cee2b3e38bb8b282e0
Vinay AVS <vinayavs@gmail.com>
2020-07-13 14:46:04 -0500

    Class Method Examples

---
+++ 02-OOPs/25-noOfObjects.py
@@ -0,0 +1,20 @@
+# CLASS METHOD EXAMPLES
+# GET NO OF OBJECTES CREATED FOR A CLASS
+
+class Test:
+    count = 0 # Static Variable
+    def __init__(self):
+        Test.count = Test.count + 1  # To access static variable inside constructor
+    @classmethod
+    def getNoOfObjects(cls):
+       print('NO OF OBJS FOR GIVEN CLASS :',cls.count)
+
+Test.getNoOfObjects()  # 0
+t1 = Test() # Obj Created, Constructor executed automatically
+t2 = Test() # Obj Created, Constructor executed automatically
+t2 = Test()
+t2 = Test()
+Test.getNoOfObjects()  # 4
+
+    
+
