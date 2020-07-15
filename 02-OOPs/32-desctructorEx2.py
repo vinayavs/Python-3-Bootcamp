9a0a48da4765e083f9ce75f7407bda83bb0e22a1
Vinay AVS <vinayavs@gmail.com>
2020-07-15 18:57:05 -0500

    Destructor Examples

--- 02-OOPs/32-desctructorEx2.py
+++ 02-OOPs/32-desctructorEx2.py
@@ -21,6 +21,6 @@ time.sleep(5)
 print('Removing Last Reference...')
 del t3
 time.sleep(10)  
-# Now Test() is elegible for GC, Obj destroyed after 10 secs controller reaches EOA
-# but t4 = Test() still have reference. so after 10sec at EOA - All objs are elegilble for GC, So destructor executed
+# Now Test() is eligible for GC, Obj destroyed after 10 secs controller reaches EOA
+# but t4 = Test() still have reference. so after 10sec at EOA - All objs are eligilble for GC, So destructor executed
 print('END OF THE APPLICATION')
--- 02-OOPs/33-desctructorEx.py
+++ 02-OOPs/33-desctructorEx.py
@@ -1,5 +1,5 @@
-# for every object, a desctructor will be executed seperately.
-# while running the application, if want to destroy the list we need to use GC-desctructor.
+# for every object, a destructor will be executed separately.
+# while running the application, if want to destroy the list we need to use GC-destructor.
 
 import time
 class Test:
@@ -8,11 +8,11 @@ class Test:
     def __del__(self):
         print('OBJECT DESTROYED')
 l = [Test(), Test(), Test()] # list of 3 objects
-print('list deleted, 3 objects are elegible for GC now')
+print('list deleted, 3 objects are eligible for GC now')
 del l
 time.sleep(10)
 print('END OF THE APPLICATION')
 
 # l = [Test(), Test(), Test()] # list of 3 objects
 # print('END OF THE APPLICATION')
-# print('EOA, All objects are elegible for GC now automatically')
+# print('EOA, All objects are eligible for GC now automatically')
