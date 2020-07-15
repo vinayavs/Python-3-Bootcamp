78227c2b6bf138eb7f95030db18fe154675fae29
Vinay AVS <vinayavs@gmail.com>
2020-07-15 18:17:13 -0500

    Destructor Examples

---
+++ 02-OOPs/31-destructorEx.py
@@ -0,0 +1,29 @@
+# DESTRUCTOR - Is responsible for Cleanup activities
+# Garbabe Collector - is meant for destroy the obj
+# Before going to destroy the object GC calls the destructor to cleanup db and n/w links...
+
+import gc
+import time
+class Test:
+    def __init__(self):  # Constructor
+        print('OBJ INITIALIZED')
+        print('GC status :',gc.isenabled()) # Enabled by default
+    def __del__(self):  # Desctrucor
+        print('OBJ DESCTRUCTED')
+
+t = Test()
+time.sleep(10)
+print('END OF APPLICATION') 
+# Alll the objector are eligible for DC at the end of the application, so Destructor executed after 10 secs
+
+# 
+t = Test()
+t = None  # Test() doest have any reference now, so it is elegible for DC, desctructor executed
+time.sleep(10)
+print('END OF THE APPLICATION')
+
+
+t1 = Test()
+t2 = Test()
+time.sleep(10)
+print('END OF THE APPLICATION') # 2 TIMES DESTRUCTOR EXECUTED.
---
+++ 02-OOPs/32-desctructorEx2.py
@@ -0,0 +1,23 @@
+# DESTRUCTOR EXAMPLE
+# t1, t2, t3 ALL ARE POINTING TO SAME OBJECT.
+import time
+class Test:
+    def __init__(self):
+        print('CONSTRUCTOR EXECUTED')
+    def __del__(self):
+        print('*****DESTRUCTOR EXECUTED*****')
+
+t1 = Test()
+t2 = t1
+t3 = t2
+t4 = Test()
+del t1
+print('Obj not destroyed after deleting t1')
+del t2
+print('Obj not destroyed after deleting t2')
+print('Removing Last Reference...')
+del t3
+time.sleep(10)  
+# Now Test() is elegible for GC, Obj destroyed after 10 secs controller reaches EOA
+# but t4 = Test() still have reference. so after 10sec at EOA - All objs are elegilble for GC, So destructor executed
+print('END OF THE APPLICATION')
