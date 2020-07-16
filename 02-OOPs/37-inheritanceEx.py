bf088b7d3854c6831f267fc79cc399d9182f96f1
Vinay AVS <vinayavs@gmail.com>
2020-07-16 17:57:00 -0500

    Inheritance Examples

---
+++ 02-OOPs/37-inheritanceEx.py
@@ -0,0 +1,24 @@
+# ACCESSNG PARENT MEMBERS FROM CHILD
+# IS-A RELATIONSHIP OR INHERITANCE
+class Parent:
+    a = 10
+    def __init__(self):
+        print('Parent Constructor')
+        self.b = 20  
+    def m1(self):
+        print('Parent Instance Method')
+    @classmethod
+    def m2(cls):
+        print('Parent Class Method')
+    @staticmethod
+    def m3():
+        print('Parent Static Method')
+class Child(Parent):
+    pass
+
+c = Child() # No Constructor,So Parent Constructor will be inherited
+print(c.a) # 10, Parent Class Static Variable
+print(c.b) # 20, Parent Class Instance Variable 
+c.m1()  # Parent Instance Method
+c.m2()  # Parent Class Method
+c.m3()  # Parent Static Method
